b45e74c80fb743f681daccaa97c2abdebca1766b
MikuXi <1071800102@qq.com>
2019-08-23 14:38:40 +0800

    提交了login页面

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    App <el-button type="primary" round>主要按钮</el-button>
+    <router-view></router-view>
   </div>
 </template>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,12 @@
 // 创建一个router实例  导出给main使用
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Login from '../views/login/index.vue'
+import Login from '@/views/login'
 Vue.use(VueRouter)
 const router = new VueRouter({
   // 定义路由规则 （路径 ==> 组件）
   routes: [
-    { path: '/', redirect: '/login' },
+    // { path: '/', redirect: '/login' },
     { path: '/login', name: 'login', component: Login }
   ]
 })

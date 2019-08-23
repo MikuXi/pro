fc2a771bddbc093bc254787fd6fab4fc19236924
MikuXi <1071800102@qq.com>
2019-08-23 14:28:41 +0800

    提交了登陆页面

---
+++ src/router/index.js
@@ -0,0 +1,14 @@
+// 创建一个router实例  导出给main使用
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Login from '../views/login/index.vue'
+Vue.use(VueRouter)
+const router = new VueRouter({
+  // 定义路由规则 （路径 ==> 组件）
+  routes: [
+    { path: '/', redirect: '/login' },
+    { path: '/login', name: 'login', component: Login }
+  ]
+})
+
+export default router

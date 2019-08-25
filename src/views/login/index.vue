892f7efbaf00aa8e69388f09fe0c2397ca092d09
MikuXi <1071800102@qq.com>
2019-08-25 21:19:49 +0800

    登录页面完成

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,12 +1,12 @@
 <template>
-    <div class='container'>
+    <div class='container' ref="box">
         <el-card class="my-card">
       <img src="../../assets/images/logo_index.png" alt="">
-      <el-form :model="loginForm">
-        <el-form-item>
+      <el-form ref="loginForm" :model="loginForm" :rules="loginRules">
+        <el-form-item prop="mobile">
           <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
         </el-form-item>
-        <el-form-item>
+        <el-form-item prop="code">
           <el-input v-model="loginForm.code" placeholder="请输入验证码" style="width:236px;margin-right:10px"></el-input>
           <el-button>发送验证码</el-button>
         </el-form-item>
@@ -14,7 +14,7 @@
           <el-checkbox :value="true">我已阅读并同意用户协议和隐私条款</el-checkbox>
         </el-form-item>
         <el-form-item>
-           <el-button type="primary" style="width:100%">登 录</el-button>
+           <el-button @click="login()" type="primary" style="width:100%">登 录</el-button>
         </el-form-item>
       </el-form>
     </el-card>
@@ -24,11 +24,47 @@
 <script>
 export default {
   data () {
+    const checkMobile = (rule, value, callback) => {
+      if (!/^1[3-9]\d{9}$/.test(value)) {
+        return callback(new Error('手机号不合法'))
+      }
+      callback()
+    }
     return {
       loginForm: {
-        mobile: '',
-        code: ''
+        mobile: '13911111111',
+        code: '246810'
+      },
+      loginRules: {
+        mobile: [
+          { required: true, message: '请输入手机号', trigger: 'blur' },
+          { validator: checkMobile, trigger: 'change' }
+        ],
+        code: [
+          { required: true, message: '请输入验证码', trigger: 'blur' },
+          { len: 6, message: '验证码长度6位', trigger: 'blur' }
+        ]
       }
+    }
+  },
+  methods: {
+    login () {
+      // 调用 validate 对整体表进行校验
+      this.$refs.loginForm.validate((valid) => {
+        if (valid) {
+          // 校验成功  调用登录接口
+          this.$http.post('http://ttapi.research.itcast.cn/mp/v1_0/authorizations', this.loginForm)
+            .then(res => {
+              // 成功 跳转
+              // 注意 登录 不够完善
+              this.$router.push('/')
+            })
+            .catch(() => {
+              // 失败 提示
+              this.$message.error('手机号或验证码错误')
+            })
+        }
+      })
     }
   }
 }
